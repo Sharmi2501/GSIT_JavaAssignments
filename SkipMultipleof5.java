f030dbdbab9325ba47b93785fe852d6f356289db
Sharmi2501 <52880570+Sharmi2501@users.noreply.github.com>
2025-10-26 21:04:57 +0530

    Add files via upload
    Multiplication table
    Skip multiples of 5 program

---
+++ SkipMultipleof5.java
@@ -0,0 +1,19 @@
+package understandingloops;
+
+public class SkipMultipleof5 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++)
+		{
+			if(i%5==0)
+			{
+				continue;
+			}
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
